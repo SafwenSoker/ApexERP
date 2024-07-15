aa52741aebef09bb9903c14f0480bc9b1f166884
Safwen Soker <safwen.soker@pyxis.com.tn>
2024-07-16 00:21:24 +0100

    fix

--- src/app/models/self-service-portal/benefit-request.ts
+++ src/app/models/self-service-portal/benefit-request.ts
@@ -1,31 +1,64 @@
+import { Employee } from "../user-management-portal/employee.model";
 import { Benefit } from "./benefit.class";
 
 export class BenefitRequest {
     id: Number;
-    employeeId: Number;
+    employee: Employee;
     motivation: String;
-    benefit: Benefit;
+    benefitDto: Benefit;
 
-    constructor(id: Number, employeeId: Number, motivation: String, benefit: Benefit) {
+    constructor(id: Number, employee: Employee, motivation: String, benefitDto: Benefit) {
         this.id = id;
-        this.employeeId = employeeId;
+        this.employee = employee;
         this.motivation = motivation;
-        this.benefit = benefit;
+        this.benefitDto = benefitDto;
     }
 
-    getId(){
+    getId() {
         return this.id;
     }
 
-    getEmployeeId(){
-        return this.employeeId;
+    getEmployeeId() {
+        return this.employee;
     }
 
-    getMotivation(){
+    getMotivation() {
         return this.motivation;
     }
 
-    getBenefit(){
-        return this.benefit;
+    getBenefit() {
+        return this.benefitDto;
     }
 }
+
+
+
+[
+    {
+        "id": 1,
+        "employee": {
+            "userId": null,
+            "firstName": "",
+            "lastName": "",
+            "userName": "admin",
+            "email": null,
+            "password": null,
+            "roles": [
+                {
+                    "id": "5c1dd980-6629-49fe-9d25-1e9b3057fdf9",
+                    "name": "default-roles-esprit",
+                    "permissions": null
+                }
+            ],
+            "active": true
+        },
+        "motivation": null,
+        "benefitDto": {
+            "id": 2,
+            "type": "MONEY",
+            "name": "Augmentation",
+            "backgroundImageUri": "https://media.istockphoto.com/id/1490133656/photo/young-woman-using-a-laptop-while-working-from-home.webp?s=1024x1024&w=is&k=20&c=mg4cZuZQfZcBi6KBU_JyNGSFzA2ZSCOexOjep4TazLc=",
+            "description": "string"
+        }
+    }
+]
